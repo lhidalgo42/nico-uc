5c90db28838b11d80962b7c1897ddbd0ea52d058
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2017-09-12 20:49:55 -0300

    app lista

--- www/js/javascript.js
+++ www/js/javascript.js
@@ -1,8 +1,36 @@
-/**
- * Created by lhida on 29-01-2017.
- */
-$("#back").click(function () {
-    history.go(-1);
-    navigator.app.backHistory();
-    window.history.back();
+
+$(document).ready(function () {
+    $("img").css('height', ($(window).height() - 10)).css('width', $(window).width());
+    $("#back").click(function () {
+        var last = JSON.parse(window.sessionStorage.getItem('last'));
+        var go = last.pop();
+        console.log(last);
+        window.sessionStorage.setItem('last', JSON.stringify(last));
+        window.location.href = go;
+    });
+    $("a:not(#back)").click(function () {
+            var last = JSON.parse(window.sessionStorage.getItem('last'));
+            last.push(window.location.pathname);
+            console.log(last);
+            window.sessionStorage.setItem('last', JSON.stringify(last));
+    });
+
+});
+var myScroll;
+function loaded() {
+    myScroll = new iScroll('content', {
+        zoom: true,
+        zoomMax: 4,
+        onBeforeScrollStart:false
+    });
+}
+
+document.addEventListener('DOMContentLoaded', loaded, false);
+$(document).ready(function () {
+    var selects = document.getElementsByTagName('select');
+    for(var i=0;i<selects.length;i++) {
+        selects[i].addEventListener('touchstart' /*'mousedown'*/, function (e) {
+            e.stopPropagation();
+        }, false);
+    }
 });
