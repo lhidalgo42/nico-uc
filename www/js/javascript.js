1de36bfd9a3d6386b5ca7829b00d17a286792004
lhidalgo42 <lhidalgosep@gmail.com>
2018-04-17 00:08:42 -0300

    se agrega buscador

--- www/js/javascript.js
+++ www/js/javascript.js
@@ -1,56 +1,56 @@
-
 $(document).ready(function () {
     $("#content").addClass('animated').addClass('fadeIn');
     $("a").addClass('prevented');
     $("a").click(function (e) {
-        if($(this).hasClass('prevented')){
+        if ($(this).hasClass('prevented')) {
             e.preventDefault();
         }
     });
     setTimeout(function () {
+        var last = JSON.parse(window.sessionStorage.getItem('last'));
+        last.push(window.location.pathname);
+        window.sessionStorage.setItem('last', JSON.stringify(last));
         $("a").removeClass("prevented");
         $("a:not(#back)").click(function (e) {
             e.preventDefault();
-            var last = JSON.parse(window.sessionStorage.getItem('last'));
-            last.push(window.location.pathname);
-            console.log(last);
-            window.sessionStorage.setItem('last', JSON.stringify(last));
             var go = $(this).attr('href');
             $("#content").removeClass('fadeIn').addClass('fadeOut');
             setTimeout(function () {
                 window.location.href = go;
-            },200);
+            }, 200);
         });
-    },400);
+    }, 400);
 
     $("img").css('height', ($(window).height() - 10)).css('width', $(window).width());
     $("#back").click(function (e) {
         e.preventDefault();
-        var last = JSON.parse(window.sessionStorage.getItem('last'));
-        var go = last.pop();
+        let last = JSON.parse(window.sessionStorage.getItem('last'));
+        let go = last.pop();
+        go = last.pop();
         window.sessionStorage.setItem('last', JSON.stringify(last));
         $("#content").removeClass('fadeIn').addClass('fadeOut');
         setTimeout(function () {
             window.location.href = go;
-        },200);
+        }, 200);
 
     });
 
 
 });
 var myScroll;
+
 function loaded() {
     myScroll = new iScroll('content', {
         zoom: true,
         zoomMax: 4,
-        onBeforeScrollStart:false
+        onBeforeScrollStart: false
     });
 }
 
 document.addEventListener('DOMContentLoaded', loaded, false);
 $(document).ready(function () {
     var selects = document.getElementsByTagName('select');
-    for(var i=0;i<selects.length;i++) {
+    for (var i = 0; i < selects.length; i++) {
         selects[i].addEventListener('touchstart' /*'mousedown'*/, function (e) {
             e.stopPropagation();
         }, false);
